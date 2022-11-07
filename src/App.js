0bcc8c3525b8a4eb2c26a0636b14c85448cd1846
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-11-07 19:36:43 +0300

    Search Box

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import './App.css';
-import React ,{Component} from 'react'
+import "./App.css";
+import React, { Component } from "react";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-    monster : [] // İnitial State
-    }
+      monster: [], 
+      searchField : ``
+      // İnitial State
+    };
   }
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then((response) => response.json())
-    .then((users) =>{this.setState(() => {
-      return {monster : users}
-    })})
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(() => {
+          return { monster: users };
+        });
+      });
   }
   render() {
+    console.log(`render`);
+
+    const _filMonster = this.state.monster.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
     return (
       <div className="App">
-      {this.state.monster.map((monster) => {
-        return (
-          <div className="box">
-            <h1 key={monster.id}>{monster.name}</h1>
-            <h2>{monster.email}</h2>
-            <h3>{monster.address.street}</h3>
-          </div>
-        )
-      })}
+        <input
+          type="search"
+          className="search-box"
+          placeholder="Search Monster"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {return {searchField}});
+          }}
+        />
+
+        {
+          _filMonster.map((monster) => {
+            return (
+              <div  className="box" key={monster.id}>
+              <h1>Name</h1>
+              <h1>{monster.name}</h1>
+              <h2>Username</h2>
+              <h2>{monster.username}</h2>
+              </div>
+            )
+          })
+        }
       </div>
-     
-    )
+    );
   }
 }
 
